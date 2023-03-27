6e6177203c2f7f3bdc4b69e78e47013f7f2621c6
Celso TCE <celso.filgueiras@tce.ap.gov.br>
2023-03-27 10:33:24 -0300

    updated to rollup 3

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,14 @@
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
+import pkg from './package.json' assert { type: 'json' };
 
-const name = require('./package.json').main.replace(/\.js$/, '')
+const name = pkg.main.replace(/\.js$/, '');
 
 const bundle = config => ({
   ...config,
   input: 'src/index.ts',
   external: id => !/^[./]/.test(id),
-})
+});
 
 export default [
   bundle({
